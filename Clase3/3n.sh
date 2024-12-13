7f2e20de993f29c14c4743a0eb7af61024b1165a
Rodrigo Belloso <114697724+rodrigobelloso@users.noreply.github.com>
2024-12-13 13:00:31 +0100

    Añadido script 3n.sh para imprimir una pirámide de asteriscos basada en la entrada del usuario.

---
+++ Clase3/3n.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read -p "Introduce un número: " num
+
+for (( i=1; i<=num; i++ ))
+do
+    for (( j=i; j<num; j++ ))
+    do
+        echo -n " "
+    done
+    
+    for (( k=1; k<=(2*i-1); k++ ))
+    do
+        echo -n "*"
+    done
+    
+    echo ""
+done
