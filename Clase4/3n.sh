97e41c8800a1b54b0012b765ace453d24c852e02
Rodrigo Belloso <114697724+rodrigobelloso@users.noreply.github.com>
2025-01-09 11:32:26 +0100

    Añadido el script 3n.sh para buscar un archivo y extraer la tercera ocurrencia de "hola".

---
+++ Clase4/3n.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#
+# Script para buscar un archivo y escribir hola en el.
+#
+
+read -p "Introduce la ruta del fichero: " archivo
+
+if [ ! -f "$archivo" ]; then
+    echo "El archivo no existe o no es un fichero regular"
+    exit 1
+fi
+
+grep -n "hola" "$archivo" | sed -n '3p' | sed 's/^[0-9]*://'
