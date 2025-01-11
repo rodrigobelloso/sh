8124fa0a482506a908beadf98fc226259dac205d
Rodrigo Belloso <114697724+rodrigobelloso@users.noreply.github.com>
2025-01-11 18:59:12 +0100

    Añadida la carpeta de scripts personales

---
+++ Personal/rpiRestPart.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#
+# Restaura la tabla de particiones desde cualquier ubicación.
+#
+
+restaurarTablaParticiones() {
+    clear
+    sudo bash -c 'cat /mnt/share/bckp/partTable/pi/sf | sfdisk /dev/mmcblk0 --no-reread'
+    clear
+    reboot
+    exit 0
+}
+
+while true; do
+    printf "Estás a punto de restaurar la tabla de particiones mientras el sistema está en ejecución. \a\n"
+    read -r -p "¿Estás seguro de esto? [s,n]: " respuesta
+    
+    case $respuesta in
+        s|S)
+            restaurarTablaParticiones
+            ;;
+        n|N)
+            clear
+            exit 0
+            ;;
+        *)
+            echo "$respuesta es una opción inválida, por favor escribe 's' o 'n'."
+            printf "\n"
+            ;;
+    esac
+done
+
