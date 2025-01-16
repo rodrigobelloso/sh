4bf6fc5f19ed3a0ef2f962c6b973ad75ce0cd2c8
Rodrigo Belloso <114697724+rodrigobelloso@users.noreply.github.com>
2025-01-16 10:07:15 +0100

    Eliminado el script rpiRestPart.sh y añadido un nuevo script 1n.sh que saluda al usuario.

---
+++ Clase5/1n.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#
+# Script que saluda al usuario.
+#
+
+echo "Por favor, ingresa tu nombre:"
+read nombre
+
+echo "¡Hola $nombre, bienvenido al mundo de la programación en bash!"
+exit 0;
