05a08a1bb9174620845f621907f13bf78fede367
Rodrigo Belloso <114697724+rodrigobelloso@users.noreply.github.com>
2025-03-10 08:33:01 +0100

    Agrega un script para simular el lanzamiento de un dado

---
+++ Clase7/4n.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#
+# Script que simula el lanzamiento de un dado (número aleatorio del 1 al 6)
+#
+
+resultado=$(( (RANDOM % 6) + 1 ))
+
+echo "Has lanzado un dado y ha salido: $resultado"
+
+exit 0
