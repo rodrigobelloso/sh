43deb4712f7e3db7b6a11b219398eff49ed4d632
Rodrigo Belloso <114697724+rodrigobelloso@users.noreply.github.com>
2025-03-11 17:38:11 +0100

    Añade scripts 8n.sh y 9n.sh que demuestran el uso de comillas simples y dobles en Bash.

---
+++ Repaso/8n.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Uso básico de la comilla simple.
+#
+
+nombre='María'
+
+echo 'Hola $nombre, hoy es $(date)'
+
+exit 0
+
+# Output: Hola $nombre, hoy es $(date)
---
+++ Repaso/9n.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# Uso básico de la comilla simple.
+#
+
+nombre="Juan"
+
+echo "Hola $nombre, tu directorio home es $HOME"
+
+exit 0
+
+# Output: Hola Juan, tu directorio home es /home/juan
