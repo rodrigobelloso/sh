dcd962f52820e34f1068d1d4a4c483037d8d550d
Rodrigo Belloso <114697724+rodrigobelloso@users.noreply.github.com>
2025-03-25 08:17:04 +0100

    Cambia los permisos de ejecución de los scripts 1n.sh, 2n.sh y 3n.sh, y añade el nuevo script 4n.sh que determina si un número es par o impar.

---
+++ Clase8/4n.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Error: Debe proporcionar un número como parámetro."
+    exit 1
+fi
+
+if [ $(($1 % 2)) -eq 0 ]; then
+    echo "$1 es par."
+else
+    echo "$1 es impar."
+fi
