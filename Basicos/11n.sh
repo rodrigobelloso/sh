08a63db8528d574f8bad371e51b52242ea20cdc0
Rodrigo Belloso <114697724+rodrigobelloso@users.noreply.github.com>
2025-03-25 18:49:04 +0100

    Añade un parámetro de medida al script 11n.sh para incluir la altura en el saludo.

--- Basicos/11n.sh
+++ Basicos/11n.sh
@@ -6,12 +6,13 @@
 
 if [ $# -eq 0 ]; then
     echo "Error: Por favor, proporciona un nombre como parámetro."
-    echo "Uso: $0 <nombre>"
+    echo "Uso: $0 <nombre> <medida>"
     exit 1
 fi
 
 nombre=$1
+medida=$2
 
-echo "¡Hola, $nombre!"
+echo "¡Hola, $nombre! mides $medida metros."
 
 exit 0
